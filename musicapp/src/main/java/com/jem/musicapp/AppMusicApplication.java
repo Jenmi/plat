2fa00a0a62e136ebeba8e3ea8838addf6bad78a1
815262163@qq.com <815262163@qq.com>
2019-04-10 22:32:10 +0800

    更新版本

--- musicapp/src/main/java/com/jem/musicapp/AppMusicApplication.java
+++ musicapp/src/main/java/com/jem/musicapp/AppMusicApplication.java
@@ -4,7 +4,10 @@ import com.didispace.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+	"com.jem.musicapp.controller",
+	"com.jem.musicapp.config",
+})
 @EnableSwagger2Doc
 public class AppMusicApplication {
 	public static void main(String[] args){
---
+++ musicservice/src/main/java/com/jem/musicservice/mapper/MusicMapper.java
@@ -0,0 +1,9 @@
+package com.jem.musicservice.mapper;
+
+import com.jem.musicservice.po.MusicPo;
+
+/**
+ * Created by Jem on 2019/4/10 21:13
+ */
+public interface MusicMapper extends BaseMapper<MusicPo>{
+}
